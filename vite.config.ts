130721d9c4d95565542800ce62e0719aeecb451d
Erin Morelli <me@erin.dev>
2024-02-07 23:54:21 -0500

    Add app version to header, 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
-import { vitePluginVersionMark } from 'vite-plugin-version-mark';
 
 import react from '@vitejs/plugin-react';
 import path from 'path';
-
-const isProd = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,15 +14,5 @@ export default defineConfig({
       '@admin': path.resolve(__dirname, './src/client/admin'),
     },
   },
-  plugins: [
-    react(),
-    vitePluginVersionMark({
-      version: isProd
-        ? process.env.HEROKU_RELEASE_COMMIT!.slice(0, 8)
-        : undefined,
-      ifGitSHA: !isProd,
-      ifGlobal: false,
-      ifLog: false,
-    })
-  ],
+  plugins: [react()],
 });
